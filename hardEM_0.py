d6bc998258cfdbeb819bcdf4af094bd6978e3781
abcde <>
2020-01-10 16:46:13 -0600

    added clustering initialization feature as option in place of using censoring rate

--- hardEM_0.py
+++ hardEM_0.py
@@ -1,4 +1,4 @@
-#This is a Hard EM algorithm intended for a mixture model arising from survival analysis. More precisely, this is for situations when we have a cured subpopulation in our survival analysis dataset, but we do not know who is cured. In order to predict the population survival function, we need to know who is cured and instead of using the usual (soft) EM algorithm to predict who is cured via logistic regression, we use our Hard EM algorithm. The difference between the latter two is that our version assigns actual (hard) labels which indicate whether a patient is cured to the censored rows, while the soft EM assigns a probability distribution instead. Clearly, any patient belonging to a noncensored rows is not cured.
+'''This is a Hard EM algorithm intended for a mixture model arising from survival analysis. More precisely, this is for situations when we have a cured subpopulation in our survival analysis dataset, but we do not know who is cured. In order to predict the population survival function, we need to know who is cured and instead of using the usual (soft) EM algorithm to predict who is cured via logistic regression, we use our Hard EM algorithm. The difference between the latter two is that our version assigns actual (hard) labels which indicate whether a patient is cured to the censored rows, while the soft EM assigns a probability distribution instead. Clearly, any patient belonging to a noncensored rows is not cured.'''
 
 import numpy as np
 from scipy.optimize import minimize
@@ -8,6 +8,7 @@ from autograd import grad, jacobian, hessian
 import pandas as pd
 import autograd.numpy as np
 from autograd import grad
+from sklearn.cluster import KMeans
 
 
 
@@ -15,13 +16,34 @@ def sigmoid(x):
     return 0.5 * (np.tanh(x / 2.) + 1)
 
 
+def swap(x): 
+   #This one is used for when the 'use_clustering' option is selected for the initialize parameter.
+    if x==1:
+        
+        return 0
+    
+    if x==0:
+        
+        return 1
+    
+vswap = np.vectorize(swap)    
+
+
+def dist(x,y):
+    #This one is used for when the 'use_clustering' option is selected for the initialize parameter.
+    return np.sqrt(np.dot(x-y,x-y))
+    
+
+
 def prob(weights, inputs):
     # Outputs probability of a patient being cured according to logistic model.
     return sigmoid(np.dot(inputs, weights))
 
 
-def HEM_fit(censored_inputs, noncensored_inputs, C, maxiter):
+def HEM_fit(censored_inputs, noncensored_inputs, C, maxiter, initialize):
     
+    '''This function implements the algorithm by minimizing the training loss. To initialize the minimization, which is done using the constrained SLSQP method, we need to choose an initial guess for both the unknown cure labels for the censored rows, and the covariate weights. If the option 'censoring_rate' is selected, then we initialize by assuming the probability of being cured is the censoring rate, and use this to generate cure labels for the censored rows. Otherwise, if 'use_clustering' is selected then a single cluster is created from the noncensored rows, and two clusters are created from the censored rows. By comparing the distance of the two censored cluster centers to the noncensored cluster center, cure labels are assigned to the censored rows. Furthermore, the covraiate weights are initialized at random from a unifrom distribution.
+'''
     n_noncens = len(noncensored_inputs)
     n_cens = len(censored_inputs)
     n_rows = n_noncens+n_cens
@@ -63,38 +85,83 @@ def HEM_fit(censored_inputs, noncensored_inputs, C, maxiter):
     cons1 = {'type': 'ineq', 'fun': constraint1}
     cons2 = {'type': 'ineq', 'fun': constraint2}
     cons = [cons1, cons2]
+    
+    guess_weights = np.random.uniform(-0.5,0.5,len(censored_inputs[0]))
 
 
+    if initialize == 'censoring_rate':
     #Use censoring rate to initialize the minimization
 
+        p = 1-n_cens/n_rows  # number of trials, probability of each trial
+        guess_unknown_labels = np.random.binomial(1, p, n_cens)
+        # result of flipping a coin once n_cens times.
 
-    p = 1-n_cens/n_rows  # number of trials, probability of each trial
-    guess_unknown_labels = np.random.binomial(1, p, n_cens)
-    # result of flipping a coin once n_cens times.
+        
+        guess = np.concatenate((guess_weights, guess_unknown_labels), axis=None)
 
+        #options0 = {'maxiter': maxiter}
 
+        #minimizer_kwargs0 = {'method':'SLSQP', 'jac':training_gradient, 'constraints':cons, 'options':options0}
 
-    guess_weights = np.random.uniform(-0.5,0.5,len(censored_inputs[0]))
-    guess = np.concatenate((guess_weights, guess_unknown_labels), axis=None)
+        #res = basinhopping(training_loss, guess, minimizer_kwargs=minimizer_kwargs0)
 
-    #options0 = {'maxiter': maxiter}
+        res = minimize(training_loss, guess, method='SLSQP', jac=training_gradient, constraints=cons, options={'maxiter': maxiter})
 
-    #minimizer_kwargs0 = {'method':'SLSQP', 'jac':training_gradient, 'constraints':cons, 'options':options0}
-
-    #res = basinhopping(training_loss, guess, minimizer_kwargs=minimizer_kwargs0)
-
-    res = minimize(training_loss, guess, method='SLSQP', jac=training_gradient, constraints=cons, options={'maxiter': maxiter})
-
-    model_weights = res.x[0:len(censored_inputs[0])]
-    unknown = res.x[len(censored_inputs[0]):]
-    unknown_cure_labels = np.rint(unknown)
-    fit = {'model_weights':model_weights, 'unknown_cure_labels':unknown_cure_labels}
+        model_weights = res.x[0:len(censored_inputs[0])]
+        unknown = res.x[len(censored_inputs[0]):]
+        unknown_cure_labels = np.rint(unknown)
+        fit = {'model_weights':model_weights, 'unknown_cure_labels':unknown_cure_labels}
     
-    return fit
+        return fit
+
+    elif initialize == 'use_clustering':
+    #Use clustering to initialize the minimization    
+        
+        
+        kmeans_nc = KMeans(n_clusters=1, random_state=0).fit(noncensored_inputs)
+
+        noncensored_clust_cent = kmeans_nc.cluster_centers_[0]
+        
+        kmeans_c = KMeans(n_clusters=2, random_state=0).fit(censored_inputs)
+        
+        censored_clust_cent = kmeans_c.cluster_centers_
+        
+        cens_cluster1_cent = censored_clust_cent[0]
+
+        cens_cluster2_cent = censored_clust_cent[1]
+        
+        c_labels = kmeans_c.labels_
+        
+        if dist(cens_cluster1_cent, noncensored_clust_cent)<dist(cens_cluster2_cent, noncensored_clust_cent):
+    
+            c_labels = vswap(c_labels)
+        
+        guess_unknown_labels = c_labels
+        
+        guess = np.concatenate((guess_weights, guess_unknown_labels), axis=None)
+        
+        res = minimize(training_loss, guess, method='SLSQP', jac=training_gradient, constraints=cons, options={'maxiter': maxiter})
+
+        model_weights = res.x[0:len(censored_inputs[0])]
+        unknown = res.x[len(censored_inputs[0]):]
+        unknown_cure_labels = np.rint(unknown)
+        fit = {'model_weights':model_weights, 'unknown_cure_labels':unknown_cure_labels}
+    
+        return fit
+    
+    
+    else: raise ValueError("Need initialize parameter to be chosen as either 'use_clustering' or 'censoring_rate'")
+
+        
+        
 
 
 
 def HEM_predictions(model_weights, df, covariates):
+    
+    '''Gives a prediction column by rounding 1-p where p is the probablity of being cured. This is because the cure lable is 0 and 
+    the not cured label is 1. The df refers to the original datarame and covariates is the list of covariates from the dataframe
+    and in the same order, and similarly for model_weights.'''
     
     n_rows = len(df.index)
     
