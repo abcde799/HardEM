a5b6bc05ca730ba31081e45e6a0cbe10aa5ed942
abcde <>
2020-01-14 01:23:26 -0600

    printed table of test scores

--- naive.py
+++ naive.py
@@ -4,11 +4,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
 from make_df import create_df, make_inputs
+from sklearn.cluster import KMeans
+from hardEM_0 import vswap, dist
 
 
+ 
 
-
-def naive_fit(censored_inputs, noncensored_inputs):
+def naive_fit(censored_inputs, noncensored_inputs, initialize):
     
     #Note that this model gives a probability of *not* being cured, so label is 1.
     
@@ -20,42 +22,96 @@ def naive_fit(censored_inputs, noncensored_inputs):
     n_cens = len(censored_inputs)
     n_rows = n_noncens+n_cens
     
-    
-    
-    #Use censoring rate to fill in missing values. 
-
-    p = 1-n_cens/n_rows  # probability of getting cured (label 0!) is the censored rate.
-    guess_unknown_labels = np.random.binomial(1, p, n_cens)
-    # result of flipping a coin once n_cens times.
-
-        
     total_inputs = np.concatenate((censored_inputs, noncensored_inputs), axis=0)
         
     noncens_labels = np.repeat(1,n_noncens)
+    
+    if initialize == 'censoring_rate':
+    #Use censoring rate to initialize the minimization
+    
+        #Use censoring rate to fill in missing values. 
+
+        p = 1-n_cens/n_rows  # probability of getting cured (label 0!) is the censored rate.
+        guess_unknown_labels = np.random.binomial(1, p, n_cens)
+        # result of flipping a coin once n_cens times.
         
-    labels = np.concatenate((guess_unknown_labels, noncens_labels), axis=None)
+        labels = np.concatenate((guess_unknown_labels, noncens_labels), axis=None)
         
-    clf = LogisticRegression(random_state=0).fit(total_inputs, labels)
+        clf = LogisticRegression(random_state=0).fit(total_inputs, labels)
     
-    nonintercept_weights = np.ndarray.flatten(clf.coef_)
+        nonintercept_weights = np.ndarray.flatten(clf.coef_)
     
-    intercept = clf.intercept_
+        intercept = clf.intercept_
     
-    weights = np.concatenate((intercept, nonintercept_weights))
+        weights = np.concatenate((intercept, nonintercept_weights))
     
-    predictions = clf.predict(total_inputs)
+        predictions = clf.predict(total_inputs)
     
-    prob = clf.predict_proba(total_inputs)
+        prob_not_cured = clf.predict_proba(total_inputs)[:,1]
     
-    prob_not_cured = np.ndarray.flatten(prob)[1000:]
+        
     
     
-    fit = {'predictions':predictions, 'prob_not_cured':prob_not_cured}
+        fit = {'pred':predictions, 'prob':prob_not_cured}
        
         
-    return fit
+        return fit
 
 
+    elif initialize == 'use_clustering':
+    #Use clustering to initialize the minimization    
+        
+        
+        kmeans_nc = KMeans(n_clusters=1, random_state=0).fit(noncensored_inputs)
+
+        noncensored_clust_cent = kmeans_nc.cluster_centers_[0]
+        
+        kmeans_c = KMeans(n_clusters=2, random_state=0).fit(censored_inputs)
+        
+        censored_clust_cent = kmeans_c.cluster_centers_
+        
+        cens_cluster1_cent = censored_clust_cent[0]
+
+        cens_cluster2_cent = censored_clust_cent[1]
+        
+        c_labels = kmeans_c.labels_
+        
+        if dist(cens_cluster1_cent, noncensored_clust_cent)<dist(cens_cluster2_cent, noncensored_clust_cent):
+    
+            c_labels = vswap(c_labels)
+        
+        guess_unknown_labels = c_labels
+        
+        labels = np.concatenate((guess_unknown_labels, noncens_labels), axis=None)
+        
+        clf = LogisticRegression(random_state=0).fit(total_inputs, labels)
+    
+        nonintercept_weights = np.ndarray.flatten(clf.coef_)
+    
+        intercept = clf.intercept_
+    
+        weights = np.concatenate((intercept, nonintercept_weights))
+    
+        predictions = clf.predict(total_inputs)
+    
+        prob_not_cured = clf.predict_proba(total_inputs)[:,1]
+    
+        
+    
+    
+        fit = {'pred':predictions, 'prob':prob_not_cured}
+       
+        
+        return fit
+    
+    
+    else: raise ValueError("Need initialize parameter to be chosen as either 'use_clustering' or 'censoring_rate'")
+
+        
+        
+                        
+        
+        
 def get_true_labels(foo, cols):
     
     '''cols is the censoring indicator, cure label columns each having values either 0 or 1, and is specified as a list containing two            strings. E.g. ['status', 'curelabel'] in this order. So first is censoring indicator and second is cure label.'''
@@ -76,16 +132,7 @@ def get_true_labels(foo, cols):
     return true_labels                                  
 
  
-#def make_inputs(df, indicator, cols):
-    
-   
-  
-    
-  #  first = cols[0]
-   
- #   new_df = df[df[first]==indicator]
-    
-  #  drop_labels = new_df.drop(columns=cols) 
+
 
 
         
